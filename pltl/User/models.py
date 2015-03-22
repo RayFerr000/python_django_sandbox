07e87e1de38c6426e7720e443445fe3269012870
raymond ferranti <ray@raymonds-MacBook-Pro.local>
2015-03-22 19:30:54 -0400

    Created custom save method that validates before saving. Only for shell test use

--- pltl/User/models.py
+++ pltl/User/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 from django.contrib.auth.models import AbstractBaseUser
 
 class User(AbstractBaseUser):
@@ -8,5 +9,18 @@ class User(AbstractBaseUser):
     course_id = models.CharField(max_length=20, default='DEFAULT VALUE')
     role = {}
     USERNAME_FIELD = 'email'
+
+    def save(self, *args, **kwargs):
+    	error = False
+    	try:
+    		User.clean_fields(self)
+    	except ValidationError as e:
+    		print 'ValidationError!: %s' %(e)
+    		error = True
+    	if error == False:
+    		super(User,self).save(*args, **kwargs)
+
+
+
 
 # Create your models here.
